1033a34dffab7474c2bcbae3507eeaa240ef6c3b
Tom <werkzn@gmail.com>
2026-02-19 18:25:06 +0300

    Модифицируем функции добавления и проверки

--- Kommunalka_SQL/Add_and_check_data_func/f_add_payment.sql
+++ Kommunalka_SQL/Add_and_check_data_func/f_add_payment.sql
@@ -10,17 +10,31 @@ DECLARE
     v_total NUMERIC;
     v_date DATE;
 BEGIN
-        -- ВЫЗЫВАЕМ ФУНКЦИЮ ПРОВЕРКИ
+    -- Вызываем проверку (включая проверку на дубликат)
     PERFORM f_validate_payment(p_month, p_year, p_utilities);
-
+    
     v_total := v_fixed + p_utilities;
     v_date := (p_year || '-' || LPAD(p_month::TEXT, 2, '0') || '-15')::DATE;
     
-    INSERT INTO t_payments (payment_date, fixed_rent, utilities, total)
-    VALUES (v_date, v_fixed, p_utilities, v_total);
-
+    -- Вставляем с новыми колонками
+    INSERT INTO t_payments (
+        payment_date, 
+        fixed_rent, 
+        utilities, 
+        total,
+        payment_year,
+        payment_month
+    )
+    VALUES (
+        v_date, 
+        v_fixed, 
+        p_utilities, 
+        v_total,
+        p_year,
+        p_month
+    );
+    
     RAISE NOTICE 'Платеж за % успешно добавлен', TO_CHAR(v_date, 'DD.MM.YYYY');
-
 END;
 $$
 LANGUAGE plpgsql;
